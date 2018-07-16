251b55cb21d3220beec402250bdcc7d2e9c21af5
Caleb Davenport <xcmdprompt@gmail.com>
2018-07-16 11:21:43 +0000

    done

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,24 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
+        self.following = []
+        self.posts = []
+        self.timeline = []
 
     def add_post(self, post):
-        pass
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        sorted_timeline = []
+        for user in self.following:
+            for post in user.posts:
+                sorted_timeline.append(post)
+        
+        return sorted(sorted_timeline, key=lambda post: post.timestamp)
 
     def follow(self, other):
-        pass
+        self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,44 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    # specify user=None so you can pass one to self in if needed
+    def __init__(self, text, timestamp=None, user=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = user
+
+    def set_user(self, user):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post): 
+    def __init__(self, text, timestamp=None, user=None):
+        super(TextPost, self).__init__(text, timestamp, user)
+
+    def __str__(self):
+        return '@{first_name} {last_name}: "{post}"\n\t{time}'.format(first_name=self.user.first_name,
+        last_name=self.user.last_name, post=self.text, time=datetime.strftime(self.timestamp, '%A, %b %d, %Y'))
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None, user=None):
+        super(PicturePost, self).__init__(text, timestamp, user)
+        self.image_url = image_url
+
+    def __str__(self):
+        return '@{first_name} {last_name}: "{post}"\n\t{image_url}\n\t{time}'.format(first_name=self.user.first_name, 
+        last_name=self.user.last_name, post=self.text, image_url=self.image_url, time=datetime.strftime(self.timestamp, '%A, %b %d, %Y'))
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None, user=None):
+        super(CheckInPost, self).__init__(text, timestamp, user)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.user = user
+
+    def __str__(self):
+        return '@{first_name} Checked In: "{post}"\n\t{latitude}, {longitude}\n\t{time}'.format(first_name=self.user.first_name, 
+        post=self.text, latitude=self.latitude, longitude=self.longitude, time=datetime.strftime(self.timestamp, '%A, %b %d, %Y'))
+
